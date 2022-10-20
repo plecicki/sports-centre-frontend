6005d9fff57e0a9988765cdb67009392e9dc7a6a
PLecicki <p.lecicki@gmail.com>
2022-10-20 10:41:08 +0200

    First commit

---
+++ src/main/java/com/kodilla/sportscentrefront/SportsCentreFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.sportscentrefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCentreFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsCentreFrontendApplication.class, args);
+    }
+
+}

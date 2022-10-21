e3f2e1b2200660fb2ef60f5c794fd696fa4b6837
PLecicki <p.lecicki@gmail.com>
2022-10-21 18:08:51 +0200

    Example code to practise Vaadin

---
+++ src/main/java/com/kodilla/sportscentrefront/domain/Book.java
@@ -0,0 +1,75 @@
+package com.kodilla.sportscentrefront.domain;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String publicationYear;
+    private BookType type;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String publicationYear, BookType type) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public BookType getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        if (publicationYear != null ? !publicationYear.equals(book.publicationYear) : book.publicationYear != null)
+            return false;
+        return type != null ? type.equals(book.type) : book.type == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (publicationYear != null ? publicationYear.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setType(BookType type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/com/kodilla/sportscentrefront/domain/BookType.java
@@ -0,0 +1,5 @@
+package com.kodilla.sportscentrefront.domain;
+
+public enum BookType {
+    IT, THRILLER, CLASSIC, POETRY
+}
